288a97f69d3e3e0185cd172715e55b0189b09278
Amit-Bongale <amitbongale14@gmail.com>
2025-12-22 16:21:15 +0530

    images config added

--- src/main/java/com/example/KFCC_Backend/jwt/JwtFilter.java
+++ src/main/java/com/example/KFCC_Backend/jwt/JwtFilter.java
@@ -24,6 +24,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
